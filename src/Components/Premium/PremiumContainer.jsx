5a0cb990dcc4a3216bfe115a7cbb31db93d2d312
alexisbarros <apenasalexis@gmail.com>
2022-01-14 01:12:30 -0300

    update: Premium - finish module

--- src/Components/Premium/PremiumContainer.jsx
+++ src/Components/Premium/PremiumContainer.jsx
@@ -2,23 +2,60 @@ import React, { useState } from 'react';
 
 // Modules
 import PremiumView from './PremiumView';
+import { message } from 'antd';
 
 const PremiumContainer = (props) => {
 
-    const [card, setCard] = useState(
-        {
-            cvc: '',
-            expiry: '',
-            focus: '',
-            name: '',
-            number: '',
+    const [loading, setLoading] = useState(false);
+    const [plan, setPlan] = useState('month');
+
+    const subscribe = async () => {
+        setLoading(true);
+        // Get user Id
+        const url = new URLSearchParams(props.location.search);
+        await updateUserPaymentFrequency(url.get('id'), plan === 'month' ? 1 : 12);
+        // Call API
+        let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/premium/mp-link?id=${url.get('id')}&type=${plan}`,
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                method: 'GET',
+            });
+        apiResponse = await apiResponse.json();
+        // Check if response was successfuly
+        if (apiResponse.code === 200) {
+            setLoading(false);
+            window.location.replace(apiResponse.data.checkoutLink);
+        } else {
+            message.error(apiResponse.message);
+            setLoading(false);
         }
-    );
+    }
+
+    const updateUserPaymentFrequency = async (id, planNumber) => {
+        // Call API
+        let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/users/${id}`,
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                method: 'PUT',
+                body: JSON.stringify({ premiumSubscriptionFrequency: planNumber })
+            });
+        apiResponse = await apiResponse.json();
+        // Check if response was successfuly
+        if (!apiResponse.code === 200) message.error(apiResponse.message);
+    }
 
     return <PremiumView
+        loading={loading}
+        selectPlan={plan => setPlan(plan)}
+        plan={plan}
 
-        card={card}
-        setCard={e => setCard(e)}
+        subscribe={() => subscribe()}
     />
 }
 
--- src/Components/Premium/PremiumView.jsx
+++ src/Components/Premium/PremiumView.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-// import Cards from 'react-credit-cards';
 import './PremiumStyle.css';
 import icon from '../../assets/img/icon.png';
-// import InputMask from "react-input-mask";
-// import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
 import Checkbox from '@mui/material/Checkbox';
-// import { Divider } from 'antd';
 
 const PremiumView = (props) => {
     return (
@@ -27,185 +23,25 @@ const PremiumView = (props) => {
 
                 <div style={{ height: '30px' }} />
 
-                <div className='premiumPriceText'><Checkbox /> R$12,69  <span style={{ fontSize: 'small', fontWeight: 'normal', color: 'gray' }}> / plano mensal</span></div>
-                <div className='premiumPriceText'><Checkbox />R$126,90 <span style={{ fontSize: 'small', fontWeight: 'normal', color: 'gray' }}> / plano anual</span></div>
+                <div className='premiumPriceText' onClick={() => props.selectPlan('month')}>
+                    <Checkbox checked={props.plan === 'month'} onChange={() => props.selectPlan('month')} /> R$12,69  <span style={{ fontSize: 'small', fontWeight: 'normal', color: 'gray' }}> / plano mensal</span>
+                </div>
+                <div className='premiumPriceText' onClick={() => props.selectPlan('year')}>
+                    <Checkbox checked={props.plan === 'year'} onChange={() => props.selectPlan('year')} /> R$126,90 <span style={{ fontSize: 'small', fontWeight: 'normal', color: 'gray' }}> / plano anual</span>
+                </div>
             </div>
             <div style={{ height: '10vh', background: 'white' }} />
 
-            <Button
+            <LoadingButton
                 variant="outlined"
                 size="large"
                 fullWidth
                 style={{ position: 'absolute', width: '90%', margin: '5%', bottom: '0px' }}
-            >Assinar</Button>
-            {/* <div className='premiumBody'>
-
-
-                <div id="PaymentForm">
-                    <Cards
-                        cvc={props.card.cvc}
-                        expiry={props.card.expiry}
-                        focused={props.card.focus}
-                        name={props.card.name}
-                        number={props.card.number}
-                        placeholders={{
-                            name: 'NOME'
-                        }}
-                    />
-                    <div style={{ height: '20px' }} />
-
-                    <div className='premiumCardInfo'>
-                        <InputMask
-                            mask="9999 9999 9999 9999"
-                            disabled={false}
-                            maskChar=" "
-                            value={props.card.number}
-                            onChange={e => props.setCard({ ...props.card, number: e.target.value })}
-                            onFocus={e => props.setCard({ ...props.card, focus: 'number' })}
-                        >
-                            {(inputProps) =>
-                                <TextField
-                                    id="outlined-basic"
-                                    label="Número do cartão"
-                                    variant="outlined"
-                                    fullWidth
-                                    InputProps={{
-                                        style: {
-                                            fontFamily: "Poiret One"
-                                        }
-                                    }}
-                                    InputLabelProps={{
-                                        style: {
-                                            fontFamily: "Poiret One"
-                                        },
-                                    }}
-                                    {...inputProps}
-                                />
-                            }
-                        </InputMask>
-                        <div style={{ height: '20px' }} />
-
-                        <form>
-
-                            <TextField
-                                id="outlined-basic"
-                                label="Nome"
-                                variant="outlined"
-                                fullWidth
-                                InputProps={{
-                                    style: {
-                                        fontFamily: "Poiret One"
-                                    }
-                                }}
-                                InputLabelProps={{
-                                    style: {
-                                        fontFamily: "Poiret One"
-                                    },
-                                }}
-                                value={props.card.name}
-                                onChange={e => props.setCard({ ...props.card, name: e.target.value })}
-                                onFocus={e => props.setCard({ ...props.card, focus: 'name' })}
-                            />
-                            <div style={{ height: '20px' }} />
-
-                            <InputMask
-                                mask="99/99"
-                                disabled={false}
-                                maskChar=" "
-                                value={props.card.expiry}
-                                onChange={e => props.setCard({ ...props.card, expiry: e.target.value })}
-                                onFocus={e => props.setCard({ ...props.card, focus: 'expiry' })}
-                            >
-                                {(inputProps) =>
-                                    <TextField
-                                        id="outlined-basic"
-                                        label="Validade"
-                                        variant="outlined"
-                                        InputProps={{
-                                            style: {
-                                                fontFamily: "Poiret One"
-                                            }
-                                        }}
-                                        InputLabelProps={{
-                                            style: {
-                                                fontFamily: "Poiret One"
-                                            },
-                                        }}
-                                        style={{ width: '49%' }}
-                                        {...inputProps}
-                                    />
-                                }
-                            </InputMask>
-
-                            <InputMask
-                                mask="9999"
-                                disabled={false}
-                                maskChar=" "
-                                value={props.card.cvc}
-                                onChange={e => props.setCard({ ...props.card, cvc: e.target.value })}
-                                onFocus={e => props.setCard({ ...props.card, focus: 'cvc' })}
-                            >
-                                {(inputProps) =>
-                                    <TextField
-                                        id="outlined-basic"
-                                        label="CVC"
-                                        variant="outlined"
-                                        InputProps={{
-                                            style: {
-                                                fontFamily: "Poiret One"
-                                            }
-                                        }}
-                                        InputLabelProps={{
-                                            style: {
-                                                fontFamily: "Poiret One"
-                                            },
-                                        }}
-                                        style={{ width: '49%', float: 'right' }}
-                                        {...inputProps}
-                                    />
-                                }
-                            </InputMask>
-
-                            <Divider />
-
-                            <TextField
-                                id="outlined-basic"
-                                label="Cupom de desconto"
-                                variant="outlined"
-                                fullWidth
-                                InputProps={{
-                                    style: {
-                                        fontFamily: "Poiret One"
-                                    }
-                                }}
-                                InputLabelProps={{
-                                    style: {
-                                        fontFamily: "Poiret One"
-                                    },
-                                }}
-                                value={props.card.name}
-                                onChange={e => props.setCard({ ...props.card, name: e.target.value })}
-                            />
-
-                            <Divider />
-
-                            <div style={{ textAlign: 'center' }}>
-                                <div className='premiumSubTitle'>Total: R$ 10,58 <span style={{ fontSize: 'x-small' }}>/ mês</span></div>
-                            </div>
-
-                            <Divider />
-
-                            <Button
-                                variant="contained"
-                                size="large"
-                                fullWidth
-                            >Assinar</Button>
-                            <div style={{ height: '20px' }} />
-
-                        </form>
-                    </div>
-                </div>
-            </div> */}
+                loading={props.loading}
+                loadingPosition="start"
+                startIcon={<div />}
+                onClick={() => props.subscribe()}
+            >Assinar</LoadingButton>
         </div>
     )
 }
