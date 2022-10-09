82c50305da6b8a3a0f5b4d891e8dec7b1feef3d3
alexisbarros <apenasalexis@gmail.com>
2022-10-09 11:58:26 -0300

    feat: show room types disableds in room tasks

--- src/Components/RoomTasks/List/ListContainer.jsx
+++ src/Components/RoomTasks/List/ListContainer.jsx
@@ -6,6 +6,7 @@ import { message } from 'antd';
 
 // Components
 import ListView from './ListView';
+import { apiRequestGet } from '../../../utils/api-request';
 
 const ListContainer = (props) => {
 
@@ -19,46 +20,29 @@ const ListContainer = (props) => {
         setLoading(true);
 
         // Call API
-        let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/room-tasks`,
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'access_token': sessionStorage.getItem('access_token') || localStorage.getItem('access_token')
-                },
-                method: 'GET',
-            });
-        apiResponse = await apiResponse.json();
+        const roomTasks = await apiRequestGet('/room-tasks');
 
-        // Check if response was successfuly
-        if (apiResponse.code === 200) {
+        if (roomTasks) {
 
             // Call API to get all room types
-            let apiRoomTypesResponse = await fetch(`${process.env.REACT_APP_API_URL}/room-types`,
-                {
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'access_token': sessionStorage.getItem('access_token') || localStorage.getItem('access_token')
-                    },
-                    method: 'GET',
-                });
-            apiRoomTypesResponse = await apiRoomTypesResponse.json();
-            let roomTypes = apiRoomTypesResponse.data.map(el => {
-                return {
-                    '_id': el['_id'],
-                    'name': el['name'],
-                    'tasks': el['tasks'].map(task => task['_id'])
-                }
-            });
-            setRoomTypes([...roomTypes]);
+            const roomTypesFound = await apiRequestGet('/room-types?withDisabled=true');
+            if (roomTypesFound) {
 
-            setData([...apiResponse.data]);
-            setLoading(false);
+                let roomTypes = roomTypesFound.map(el => {
+                    return {
+                        '_id': el['_id'],
+                        'name': el['name'],
+                        'tasks': el['tasks'].map(task => task['_id'])
+                    }
+                });
+                setRoomTypes([...roomTypes]);
+
+                setData([...roomTasks]);
+                setLoading(false);
+            }
 
         } else {
 
-            message.error(apiResponse.message);
             setLoading(false);
 
         }
