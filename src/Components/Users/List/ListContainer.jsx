be8ee7114af2d247b12eebb6c0568803ef83b940
Alexis Barros <apenasalexis@gmail.com>
2023-12-26 21:28:10 -0300

    :sparkles: add totalUsers

--- src/Components/Users/List/ListContainer.jsx
+++ src/Components/Users/List/ListContainer.jsx
@@ -1,131 +1,133 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 // Modules
-import { message } from 'antd';
-
+import { message } from "antd";
 
 // Components
-import ListView from './ListView';
+import ListView from "./ListView";
 
 const ListContainer = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [allData, setAllData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(null);
+  const [totalUsers, setTotalUsers] = useState(0);
 
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
-    const [allData, setAllData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState(null);
+  const getData = async () => {
+    setLoading(true);
 
-    const getData = async () => {
-        setLoading(true);
+    // Call API
+    let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/users`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        access_token:
+          sessionStorage.getItem("access_token") ||
+          localStorage.getItem("access_token"),
+      },
+      method: "GET",
+    });
+    apiResponse = await apiResponse.json();
 
-        // Call API
-        let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/users`,
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'access_token': sessionStorage.getItem('access_token') || localStorage.getItem('access_token')
-                },
-                method: 'GET',
-            });
-        apiResponse = await apiResponse.json();
+    if (apiResponse.code === 200) {
+      apiResponse.data.users = apiResponse.data.users.map((el) => {
+        if (typeof el.premiumFreebie === "string")
+          el.premiumFreebie = el.premiumFreebie === "true" ? true : false;
 
-        if (apiResponse.code === 200) {
-            apiResponse.data.users = apiResponse.data.users.map(el => {
-                if(typeof el.premiumFreebie === 'string')
-                    el.premiumFreebie = el.premiumFreebie === 'true' ? true : false
-                
-                return el;
-            })
+        return el;
+      });
 
-            setData([...apiResponse.data.users]);
-            setLoading(false);
-            setAllData([...apiResponse.data.users]);
-        } else {
-            message.error(apiResponse.message);
-            setLoading(false);
-        }
+      setData([...apiResponse.data.users]);
+      setLoading(false);
+      setAllData([...apiResponse.data.users]);
+      setTotalUsers(apiResponse.data.users.length);
+    } else {
+      message.error(apiResponse.message);
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    getData();
+  }, []);
 
-        getData();
+  /**
+   * Method to remove.
+   * @param {String} id
+   */
+  const removeData = async (id) => {
+    // Call API
+    let apiResponse = await fetch(
+      `${process.env.REACT_APP_API_URL}/users/${id}`,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          access_token:
+            sessionStorage.getItem("access_token") ||
+            localStorage.getItem("access_token"),
+        },
+        method: "DELETE",
+      }
+    );
+    apiResponse = await apiResponse.json();
 
-    }, [])
-
-    /**
-     * Method to remove.
-     * @param {String} id
-     */
-    const removeData = async (id) => {
-
-        // Call API
-        let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/users/${id}`,
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'access_token': sessionStorage.getItem('access_token') || localStorage.getItem('access_token')
-                },
-                method: 'DELETE',
-            });
-        apiResponse = await apiResponse.json();
-
-        // Check if response was successfuly
-        if (apiResponse.code === 200) {
-
-            getData()
-
-        } else {
-
-            message.error(apiResponse.message);
-
-        }
-
+    // Check if response was successfuly
+    if (apiResponse.code === 200) {
+      getData();
+    } else {
+      message.error(apiResponse.message);
     }
+  };
 
-    const search = async () => {
-        setLoading(true);
+  const search = async () => {
+    setLoading(true);
 
-        const dataFiltered = allData.filter(el => {
-            return (
-                (el.email || '').toLowerCase().includes((searchTerm || '').toLowerCase()) ||
-                (el.username || '').toLowerCase().includes((searchTerm || '').toLowerCase()) ||
-                (el.userType || '').toLowerCase().includes((searchTerm || '').toLowerCase())
-            )
-        })
-        setData([...dataFiltered]);
+    const dataFiltered = allData.filter((el) => {
+      return (
+        (el.email || "")
+          .toLowerCase()
+          .includes((searchTerm || "").toLowerCase()) ||
+        (el.username || "")
+          .toLowerCase()
+          .includes((searchTerm || "").toLowerCase()) ||
+        (el.userType || "")
+          .toLowerCase()
+          .includes((searchTerm || "").toLowerCase())
+      );
+    });
+    setData([...dataFiltered]);
+    setTotalUsers(dataFiltered.length);
 
-        setLoading(false);
+    setLoading(false);
+  };
+
+  /**
+   * Method to convert a string to a phone number
+   * @param {String} phoneNumberString
+   */
+  const stringToPhone = (phoneNumberString) => {
+    var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+    var match = cleaned.match(/^(1|)?(\d{2})(\d{5})(\d{4})$/);
+    if (match) {
+      var intlCode = match[1] ? "+1 " : "";
+      return [intlCode, "(", match[2], ") ", match[3], "-", match[4]].join("");
     }
+    return null;
+  };
 
-    /**
-     * Method to convert a string to a phone number
-     * @param {String} phoneNumberString
-     */
-    const stringToPhone = (phoneNumberString) => {
-        var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
-        var match = cleaned.match(/^(1|)?(\d{2})(\d{5})(\d{4})$/);
-        if (match) {
-            var intlCode = (match[1] ? '+1 ' : '');
-            return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
-        }
-        return null;
-    }
+  return (
+    <ListView
+      loading={loading}
+      data={data}
+      removeData={(id) => removeData(id)}
+      setSearchTerm={(e) => setSearchTerm(e)}
+      search={() => search()}
+      stringToPhone={(phoneNumberString) => stringToPhone(phoneNumberString)}
+      totalUsers={totalUsers}
+      setTotalUsers={(e) => setTotalUsers(e)}
+    />
+  );
+};
 
-    return (
-
-        <ListView
-
-            loading={loading}
-            data={data}
-            removeData={id => removeData(id)}
-            setSearchTerm={(e) => setSearchTerm(e)}
-            search={() => search()}
-            stringToPhone={(phoneNumberString) => stringToPhone(phoneNumberString)}
-        />
-
-    )
-
-}
-
-export default ListContainer
+export default ListContainer;
--- src/Components/Users/List/ListView.jsx
+++ src/Components/Users/List/ListView.jsx
@@ -1,230 +1,229 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Modules
-import { Card, Table, Space, Breadcrumb, Modal, Input, Pagination, Select, Row, Col, Checkbox, Button } from 'antd';
-import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-import UserDetailsModalContainer from '../UserDetailsModal/UserDetailsModalContainer'
+import {
+  Card,
+  Table,
+  Space,
+  Breadcrumb,
+  Modal,
+  Input,
+  Row,
+  Col,
+  Button,
+} from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import UserDetailsModalContainer from "../UserDetailsModal/UserDetailsModalContainer";
 
 // Styles
-import './ListStyle.css';
+import "./ListStyle.css";
 
 const ListView = (props) => {
+  const [filters, setFilters] = useState({
+    userType: [],
+    personalDataPhone: [],
+  });
 
-    const [filters, setFilters] = useState({ userType: [], personalDataPhone: [] })
+  // Columns of the table list
+  const columns = [
+    {
+      title: "E-mail",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Nome de usuário",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Tipo de usuário",
+      dataIndex: "userType",
+      key: "userType",
+      render: (e) => {
+        if (e === "free") return "Gratuito";
+        if (e === "premium") return "Premium";
+        return "-";
+      },
+      filters: [
+        { text: "Gratuito", value: "free" },
+        { text: "Premium", value: "premium" },
+      ],
+      onFilter: (value, record) => {
+        if ((record && record.userType) === value) return true;
+        return false;
+      },
+    },
+    {
+      title: "Cortesia",
+      dataIndex: "premiumFreebie",
+      key: "premiumFreebie",
+      render: (e) => {
+        if (e) return "Sim";
+        return "";
+      },
+      filters: [{ text: "Cortesia", value: true }],
+      onFilter: (value, record) => {
+        if (value === true && record && record.premiumFreebie) return true;
+        return false;
+      },
+    },
+    {
+      title: "Ativo",
+      dataIndex: "premiumValidatedDate",
+      key: "premiumValidatedDate",
+      render: (e) => {
+        if (e && new Date(e).getTime() > new Date().getTime()) return "Sim";
 
-    // Columns of the table list
-    const columns = [
-        {
-            title: 'E-mail',
-            dataIndex: 'email',
-            key: 'email',
-        },
-        {
-            title: 'Nome de usuário',
-            dataIndex: 'username',
-            key: 'username',
-        },
-        {
-            title: 'Tipo de usuário',
-            dataIndex: 'userType',
-            key: 'userType',
-            render: (e) => {
-                if (e === 'free') return 'Gratuito';
-                if (e === 'premium') return 'Premium';
-                return '-';
-            },
-            filters: [
-                { text: 'Gratuito', value: 'free' },
-                { text: 'Premium', value: 'premium' },
-            ],
-            onFilter: (value, record) => {
-                if ((record && record.userType) === value) return true;
-                return false;
-            }
-        },
-        {
-            title: 'Cortesia',
-            dataIndex: 'premiumFreebie',
-            key: 'premiumFreebie',
-            render: (e) => {
-                if (e) return 'Sim'
-                return '';
-            },
-            filters: [
-                { text: 'Cortesia', value: true },
-            ],
-            onFilter: (value, record) => {
-                if (
-                    value === true &&
-                    (record && record.premiumFreebie)
-                ) return true;
-                return false;
-            },
-        },
-        {
-            title: 'Ativo',
-            dataIndex: 'premiumValidatedDate',
-            key: 'premiumValidatedDate',
-            render: (e) => {
-                if (
-                    e &&
-                    new Date(e).getTime() > new Date().getTime()
-                ) return 'Sim';
+        return "";
+      },
+      filters: [{ text: "Ativo", value: true }],
+      onFilter: (value, record) => {
+        if (
+          value === true &&
+          record &&
+          new Date(record.premiumValidatedDate).getTime() > new Date().getTime()
+        )
+          return true;
+        return false;
+      },
+    },
+    {
+      title: "Tem empregada",
+      dataIndex: "personalData",
+      key: "hasHousekeeper",
+      render: (e) => {
+        if (e && e.hasHousekeeper) return "Sim";
+      },
+      filters: [{ text: "Sim", value: true }],
+      onFilter: (value, record) => {
+        if (
+          value === true &&
+          record &&
+          record.personalData &&
+          record.personalData.hasHousekeeper
+        )
+          return true;
+        return false;
+      },
+    },
+    {
+      title: "Telefone",
+      dataIndex: "personalData",
+      key: "personalDataPhone",
+      width: 200,
+      align: "center",
+      filters: [{ text: "Sim", value: true }],
+      onFilter: (value, record) => {
+        if (
+          value === true &&
+          record &&
+          record.personalData &&
+          record.personalData.phone
+        )
+          return true;
+        return false;
+      },
+      render: (e) => {
+        if (e && e.phone) return props.stringToPhone(e.phone);
+      },
+    },
+    {
+      title: "Ações",
+      key: "actions",
+      width: 100,
+      render: (text, record) => (
+        <Space size="middle">
+          <UserDetailsModalContainer
+            userData={record}
+            stringToPhone={(e) => props.stringToPhone(e)}
+          />
 
-                return '';
-            },
-            filters: [
-                { text: 'Ativo', value: true },
-            ],
-            onFilter: (value, record) => {
-                if (
-                    value === true &&
-                    (
-                        record && 
-                        new Date(record.premiumValidatedDate).getTime() > new Date().getTime()
-                    )
-                ) return true;
-                return false;
-            },
-        },
-        {
-            title: 'Tem empregada',
-            dataIndex: 'personalData',
-            key: 'hasHousekeeper',
-            render: (e) => {
-                if (e && e.hasHousekeeper) return 'Sim';
-            },
-            filters: [
-                { text: 'Sim', value: true },
-            ],
-            onFilter: (value, record) => {
-                if (
-                    value === true &&
-                    (record && record.personalData && record.personalData.hasHousekeeper)
-                ) return true;
-                return false;
-            },
-        },
-        {
-            title: 'Telefone',
-            dataIndex: 'personalData',
-            key: 'personalDataPhone',
-            width: 200,
-            align: 'center',
-            filters: [
-                { text: 'Sim', value: true },
-            ],
-            onFilter: (value, record) => {
-                if (
-                    value === true &&
-                    (record && record.personalData && record.personalData.phone)
-                ) return true;
-                return false;
-            },
-            render: (e) => {
-                if (e && e.phone) return props.stringToPhone(e.phone);
-            }
-        },
-        {
-            title: 'Ações',
-            key: 'actions',
-            width: 100,
-            render: (text, record) => (
+          <span
+            style={{
+              color: "red",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              Modal.confirm({
+                title: "Tem certeza que deseja excluir esse registro?",
+                icon: <ExclamationCircleOutlined />,
+                content: "Essa ação não poderá ser desfeita",
+                okText: "Sim",
+                okType: "danger",
+                cancelText: "Não",
+                onOk() {
+                  props.removeData(record._id);
+                },
+              });
+            }}
+          >
+            Deletar
+          </span>
+        </Space>
+      ),
+    },
+  ];
 
-                <Space size="middle">
+  return (
+    <div className="home-out-card">
+      <div className="home-in-card">
+        <Card title="Usuários">
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link to="/home">Início</Link>
+            </Breadcrumb.Item>
 
-                    <UserDetailsModalContainer userData={record} stringToPhone={(e) => props.stringToPhone(e)} />
+            <Breadcrumb.Item>Usuários</Breadcrumb.Item>
+          </Breadcrumb>
+          <br />
+          <br />
 
-                    <span
-                        style={{
-                            color: 'red',
-                            cursor: 'pointer'
-                        }}
-                        onClick={() => {
-
-                            Modal.confirm({
-                                title: 'Tem certeza que deseja excluir esse registro?',
-                                icon: <ExclamationCircleOutlined />,
-                                content: 'Essa ação não poderá ser desfeita',
-                                okText: 'Sim',
-                                okType: 'danger',
-                                cancelText: 'Não',
-                                onOk() {
-                                    props.removeData(record._id)
-                                },
-                            });
-
-                        }}
-                    >Deletar</span>
-                </Space>
-
-            ),
-        },
-    ];
-
-    return (
-
-        <div className='home-out-card'>
-
-            <div className='home-in-card'>
-
-                <Card
-                    title='Usuários'
+          <Row gutter={24}>
+            <Col span={20}>
+              <Input.Search
+                placeholder="Valor da buscar"
+                onChange={(e) => props.setSearchTerm(e.target.value)}
+                onSearch={() => props.search()}
+                style={{ float: "left", clear: "both", marginBottom: "20px" }}
+              />
+            </Col>
+            <Col span={4}>
+              <Button className="user-list-print-button" type="primary">
+                <Link
+                  to={{
+                    pathname: "/print-user-list",
+                    state: { data: props.data, filters: filters },
+                  }}
                 >
+                  Imprimir lista
+                </Link>
+              </Button>
+            </Col>
+          </Row>
 
-                    <Breadcrumb>
-
-                        <Breadcrumb.Item>
-                            <Link to='/home'>Início</Link>
-                        </Breadcrumb.Item>
-
-                        <Breadcrumb.Item>
-                            Usuários
-                        </Breadcrumb.Item>
-
-                    </Breadcrumb><br /><br />
-
-                    <Row gutter={24}>
-                        <Col span={20}>
-                            <Input.Search
-                                placeholder='Valor da buscar'
-                                onChange={e => props.setSearchTerm(e.target.value)}
-                                onSearch={() => props.search()}
-                                style={{ float: 'left', clear: 'both', marginBottom: '20px' }}
-                            />
-                        </Col>
-                        <Col span={4}>
-                            <Button className='user-list-print-button' type='primary'>
-                                <Link
-                                    to={{
-                                        pathname: "/print-user-list",
-                                        state: { data: props.data, filters: filters }
-                                    }}
-                                >Imprimir lista</Link>
-                            </Button>
-                        </Col>
-                    </Row>
-
-                    <Table
-                        dataSource={props.data}
-                        columns={columns}
-                        loading={props.loading}
-                        locale={{
-                            emptyText: 'Sem registros'
-                        }}
-                        onChange={(pagination, filters) => {
-                            setFilters(filters)
-                        }}
-                    />
-                </Card>
-
-            </div>
-
-        </div>
-
-    )
-
-}
+          <Table
+            dataSource={props.data}
+            columns={columns}
+            loading={props.loading}
+            locale={{
+              emptyText: "Sem registros",
+            }}
+            onChange={(pagination, filters, sorter, currentState) => {
+              setFilters(filters);
+              props.setTotalUsers(currentState.currentDataSource.length);
+            }}
+            footer={() => (
+              <div style={{ textAlign: "right", marginTop: "16px" }}>
+                Total de usuários: {props.totalUsers.toLocaleString("pt-BR")}
+              </div>
+            )}
+          />
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default ListView;
