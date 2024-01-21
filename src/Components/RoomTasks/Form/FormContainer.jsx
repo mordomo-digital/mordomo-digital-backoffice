9386dcfcd1f80d0125f078b07b62de11f1c30fa2
Alexis Barros <apenasalexis@gmail.com>
2024-01-21 01:22:30 +0000

    :art: add description, videoUrl and steps in room tasks form

--- src/Components/RoomTasks/Form/FormContainer.jsx
+++ src/Components/RoomTasks/Form/FormContainer.jsx
@@ -1,162 +1,175 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 // Modules
-import { message } from 'antd';
-
+import { message } from "antd";
 
 // Components
-import FormView from './FormView';
+import FormView from "./FormView";
 
 const FormContainer = (props) => {
+  props = props.parent_props;
 
-    props = props.parent_props;
+  /**
+   * Set form.
+   */
+  const [form, setForm] = useState({
+    name: "",
+    description: "",
+    defaultFrequency: [],
+    needToHireLabor: false,
+    videoUrl: '',
+    steps: [],
+  });
 
+  const [roomTypes, setRoomTypes] = useState([]);
+
+  const [stepToAdd, setStepToAdd] = useState(null);
+
+  const [idToUpdate, setIdToUpdate] = useState(null);
+  useEffect(() => {
     /**
-     * Set form.
+     * Get data to update.
      */
-    const [form, setForm] = useState({ name: '', defaultFrequency: [], needToHireLabor: false });
+    async function getDataToUpdate(id, localRoomTypes) {
+      // Call API.
+      let apiResponse = await fetch(
+        `${process.env.REACT_APP_API_URL}/room-tasks/${id}`,
+        {
+          headers: {
+            access_token:
+              sessionStorage.getItem("access_token") ||
+              localStorage.getItem("access_token"),
+          },
+          method: "GET",
+        }
+      );
+      apiResponse = await apiResponse.json();
 
-    const [roomTypes, setRoomTypes] = useState([]);
+      // Check if response was successfuly
+      if (apiResponse.code === 200) {
+        setForm({
+          name: apiResponse.data["name"],
+        });
 
-    const [idToUpdate, setIdToUpdate] = useState(null);
-    useEffect(() => {
+        let arrayOfRoomTypes = [];
+        let arrayOfDefaultFrequency =
+          apiResponse.data["defaultFrequency"] || [];
+        for (let index = 0; index < localRoomTypes.length; index++) {
+          if (localRoomTypes[index]["tasks"].includes(id)) {
+            let roomTypeToAdd = {
+              roomId: localRoomTypes[index]["_id"],
+              roomName: localRoomTypes[index]["name"],
+            };
+            arrayOfRoomTypes.push(roomTypeToAdd);
 
-        /**
-         * Get data to update.
-         */
-        async function getDataToUpdate(id, localRoomTypes) {
+            if (
+              arrayOfDefaultFrequency.length === 0 ||
+              (arrayOfDefaultFrequency[index] &&
+                arrayOfDefaultFrequency[index]["roomType"]) !==
+                roomTypeToAdd["roomId"]
+            ) {
+              let defaultFrequencyToAdd = {
+                roomType: localRoomTypes[index]["_id"],
+                frequency: "",
+                weekdays: [],
+                day: "",
+                startMonth: "",
+                date: undefined,
+                weekOfTheMonth: "",
+              };
 
-            // Call API.
-            let apiResponse = await fetch(`${process.env.REACT_APP_API_URL}/room-tasks/${id}`,
-                {
-                    headers: {
-                        'access_token': sessionStorage.getItem('access_token') || localStorage.getItem('access_token'),
-                    },
-                    method: 'GET',
-                });
-            apiResponse = await apiResponse.json();
-
-            // Check if response was successfuly
-            if (apiResponse.code === 200) {
-                setForm({
-                    name: apiResponse.data['name'],
-                });
-
-                let arrayOfRoomTypes = [];
-                let arrayOfDefaultFrequency = apiResponse.data['defaultFrequency'] || [];
-                for (let index = 0; index < localRoomTypes.length; index++) {
-                    if (localRoomTypes[index]['tasks'].includes(id)) {
-                        let roomTypeToAdd = {
-                            roomId: localRoomTypes[index]['_id'],
-                            roomName: localRoomTypes[index]['name'],
-                        };
-                        arrayOfRoomTypes.push(roomTypeToAdd);
-
-                        if (arrayOfDefaultFrequency.length === 0 ||
-                            (arrayOfDefaultFrequency[index] && arrayOfDefaultFrequency[index]['roomType']) !== roomTypeToAdd['roomId']
-                        ) {
-                            let defaultFrequencyToAdd = {
-                                roomType: localRoomTypes[index]['_id'],
-                                frequency: '',
-                                weekdays: [],
-                                day: '',
-                                startMonth: '',
-                                date: undefined,
-                                weekOfTheMonth: '',
-                            };
-
-                            if (arrayOfDefaultFrequency.length === 0) arrayOfDefaultFrequency.push(defaultFrequencyToAdd);
-                            else arrayOfDefaultFrequency.splice(index, 0, defaultFrequencyToAdd);
-                        }
-                    }
-                }
-                setForm({
-                    name: apiResponse.data['name'],
-                    defaultFrequency: arrayOfDefaultFrequency
-                });
-                setRoomTypes([...arrayOfRoomTypes]);
-
-            } else {
-
-                message.error(apiResponse.message);
-
+              if (arrayOfDefaultFrequency.length === 0)
+                arrayOfDefaultFrequency.push(defaultFrequencyToAdd);
+              else
+                arrayOfDefaultFrequency.splice(index, 0, defaultFrequencyToAdd);
             }
+          }
         }
-
-        /**
-         * Check if update or create form
-         */
-        if (props.location.state) {
-            setIdToUpdate(props.location.state.id)
-            getDataToUpdate(props.location.state.id, props.location.state.roomTypes);
-        }
-    }, [props.location.state])
-
-    /**
-     * Save.
-     */
-    const [loadingSaveButton, setLoadingSaveButton] = useState(false);
-    const save = async () => {
-
-        setLoadingSaveButton(true);
-
-        // Method
-        let method = idToUpdate ? 'PUT' : 'POST';
-        let endpoint = idToUpdate ? `${process.env.REACT_APP_API_URL}/room-tasks/${idToUpdate}` : `${process.env.REACT_APP_API_URL}/room-tasks`;
-
-        let Form = {
-            'name': form.name,
-            'defaultFrequency': JSON.stringify(form.defaultFrequency),
-            'needToHireLabor': form.needToHireLabor,
-        }
-
-        // Call API.
-        let apiResponse = await fetch(endpoint,
-            {
-                headers: {
-                    'access_token': sessionStorage.getItem('access_token') || localStorage.getItem('access_token'),
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: method,
-                body: JSON.stringify(Form)
-            });
-        apiResponse = await apiResponse.json();
-
-        // Check if response was successfuly
-        if (apiResponse.code === 200) {
-
-            message.success(
-                idToUpdate ?
-                    'Registro atualizado com sucesso' :
-                    'Registro criado com sucesso'
-            );
-            setLoadingSaveButton(false);
-            props.history.push('/home/room-tasks');
-
-        } else {
-
-            setLoadingSaveButton(false);
-            message.error(apiResponse.message);
-
-        }
+        setForm({
+          name: apiResponse.data["name"],
+          description: apiResponse.data["description"],
+          videoUrl: apiResponse.data["videoUrl"],
+          steps: apiResponse.data["steps"],
+          defaultFrequency: arrayOfDefaultFrequency,
+        });
+        setRoomTypes([...arrayOfRoomTypes]);
+      } else {
+        message.error(apiResponse.message);
+      }
     }
 
-    return (
+    /**
+     * Check if update or create form
+     */
+    if (props.location.state) {
+      setIdToUpdate(props.location.state.id);
+      getDataToUpdate(props.location.state.id, props.location.state.roomTypes);
+    }
+  }, [props.location.state]);
 
-        <FormView
-            idToUpdate={idToUpdate}
-            roomTypes={roomTypes}
+  /**
+   * Save.
+   */
+  const [loadingSaveButton, setLoadingSaveButton] = useState(false);
+  const save = async () => {
+    setLoadingSaveButton(true);
 
-            form={form}
-            setForm={form => setForm({ ...form })}
+    // Method
+    let method = idToUpdate ? "PUT" : "POST";
+    let endpoint = idToUpdate
+      ? `${process.env.REACT_APP_API_URL}/room-tasks/${idToUpdate}`
+      : `${process.env.REACT_APP_API_URL}/room-tasks`;
 
-            save={() => save()}
-            loadingSaveButton={loadingSaveButton}
-        />
+    let Form = {
+      name: form.name,
+      description: form.description,
+      videoUrl: form.videoUrl,
+      defaultFrequency: JSON.stringify(form.defaultFrequency),
+      steps: JSON.stringify(form.steps),
+      needToHireLabor: form.needToHireLabor,
+    };
 
-    )
+    // Call API.
+    let apiResponse = await fetch(endpoint, {
+      headers: {
+        access_token:
+          sessionStorage.getItem("access_token") ||
+          localStorage.getItem("access_token"),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: method,
+      body: JSON.stringify(Form),
+    });
+    apiResponse = await apiResponse.json();
 
-}
+    // Check if response was successfuly
+    if (apiResponse.code === 200) {
+      message.success(
+        idToUpdate
+          ? "Registro atualizado com sucesso"
+          : "Registro criado com sucesso"
+      );
+      setLoadingSaveButton(false);
+      props.history.push("/home/room-tasks");
+    } else {
+      setLoadingSaveButton(false);
+      message.error(apiResponse.message);
+    }
+  };
+
+  return (
+    <FormView
+      idToUpdate={idToUpdate}
+      roomTypes={roomTypes}
+      form={form}
+      setForm={(form) => setForm({ ...form })}
+      stepToAdd={stepToAdd}
+      setStepToAdd={(step) => setStepToAdd(step)}
+      save={() => save()}
+      loadingSaveButton={loadingSaveButton}
+    />
+  );
+};
 
 export default FormContainer;
